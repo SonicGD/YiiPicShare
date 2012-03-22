ecb215b0d7aa3987524709be5b8b6f409da9a4ac
SonicGD <sonicgd@gmail.com>
2012-03-22 14:07:40 +0600

    Adding App

---
+++ app/info.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Георгий
+ * Date: 22.03.12
+ * Time: 13:18
+ * To change this template use File | Settings | File Templates.
+ */
+phpinfo();
+$var =rand(1, 100);
+echo($var);
+?>
