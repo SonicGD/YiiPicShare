1f63029e0525188c6eea565660c66dfa462327b4
SonicGD <sonicgd@gmail.com>
2012-03-22 17:07:06 +0600

    Add about page

---
+++ app/themes/bs/views/site/about.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Георгий
+ * Date: 22.03.12
+ * Time: 17:06
+ * To change this template use File | Settings | File Templates.
+ */
+?>
+Всем ня :)
