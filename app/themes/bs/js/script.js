779b8284a836cb6d62426bfc8c42584e1e858caa
SonicGD <sonicgd@gmail.com>
2012-04-01 20:05:26 +0600

    Fix file types

--- app/themes/bs/js/script.js
+++ app/themes/bs/js/script.js
@@ -5,22 +5,24 @@
  * Time: 16:36
  * To change this template use File | Settings | File Templates.
  */
-$('#fileupload').fileupload('option', {
-    //url: '//jquery-file-upload.appspot.com/',
-    maxFileSize:5000000,
-    acceptFileTypes:/(\.|\/)(gif|jpe?g|png)$/i,
-    resizeMaxWidth:1920,
-    resizeMaxHeight:1200
-});
+$(function () {
+    $('#fileupload').fileupload('option', {
+        //url: '//jquery-file-upload.appspot.com/',
+        maxFileSize:5000000,
+        acceptFileTypes:/(\.|\/)(gif|jpe?g|png)$/i,
+        resizeMaxWidth:1920,
+        resizeMaxHeight:1200
+    });
 // Upload server status check for browsers with CORS support:
-if ($.support.cors) {
-    $.ajax({
-        url:'//bwhost.ru/',
-        type:'HEAD'
-    }).fail(function () {
-            $('<span class="alert alert-error"/>')
-                .text('Upload server currently unavailable - ' +
-                new Date())
-                .appendTo('#fileupload');
-        });
-}
+    if ($.support.cors) {
+        $.ajax({
+            url:'//bwhost.ru/',
+            type:'HEAD'
+        }).fail(function () {
+                $('<span class="alert alert-error"/>')
+                    .text('Upload server currently unavailable - ' +
+                    new Date())
+                    .appendTo('#fileupload');
+            });
+    }
+});
